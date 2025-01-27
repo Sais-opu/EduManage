fe58353f7919a620def3e8a8a7c0e18d00f526f3
Sais-opu <saidulislamapu2024@gmail.com>
2025-01-28 01:20:55 +0600

    footer added

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './component/Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
+import Footer from './component/Footer/Footer';
 
 const Root = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
